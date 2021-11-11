1a520f47c87e503959062d569aad92a5f5ba5dd7
AdamMusa <adammusa2222@gmail.com>
2021-11-11 11:46:46 +0100

    added leaflet for our custom data

--- front/components/leaflet.vue
+++ front/components/leaflet.vue
@@ -1,9 +1,13 @@
 <template>
     <div id="map-wrap" style="height: 80vh">
         <client-only>
-            <l-map :zoom=13 :center="[55.9464418,8.1277591]">
+            <l-map :zoom=5 :center="[9.548624660911141, 7.804160193482599]">
             <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></l-tile-layer>
-            <l-marker :lat-lng="[55.9464418,8.1277591]"></l-marker>
+            <l-marker :lat-lng="[8.592724737747833, 16.074241050333463]"></l-marker>
+            <l-marker :lat-lng="[9.548624660911141, 7.804160193482599]"></l-marker>
+            <l-marker :lat-lng="[12.125553758062672, 15.055388119977]">
+            <l-description></l-description>
+            </l-marker>
             </l-map>
         </client-only>
     </div>
