9e05bdfec8c3b9cd8aaee3d03c220c43f6fdb50b
AdamMusa <adammusa2222@gmail.com>
2021-11-20 13:27:47 +0100

    added 404 page

--- front/layouts/error-layout.vue
+++ front/layouts/error-layout.vue
@@ -1,11 +1,29 @@
 <template>
-  <div>
-      Ouppps 404
-  </div>
-</template>
+    <v-parallax
+        height="810"
+        class="mt-5"
+        :src="require('~/assets/img/404.jpg')"
+      >
 
+       <v-btn
+          fixed
+          bottom
+          to="/"
+          >
+            <v-icon left>mdi-arrow-left</v-icon> <span class="font-weight-bold">Go Home</span> 
+      </v-btn>
+    </v-parallax>
+
+</template>
 <script>
 export default {
-    layout:"default"
+    layout:"error"
 }
 </script>
+
+
+<style scoped>
+ .v-btn{
+   color:#458DF3;
+ }
+</style>
