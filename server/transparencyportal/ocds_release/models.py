a98738bcae707c95b1ccdf2d28b990910fdb14c8
Harry Michael <ando.lalaina1103@gmail.com>
2021-11-21 10:27:06 +0300

    Update nullable fields on Release

--- server/transparencyportal/ocds_release/models.py
+++ server/transparencyportal/ocds_release/models.py
@@ -4,21 +4,22 @@ from ocds_master_tables.models import Entity
 from ocds_tender.models import Tender
 from ocds_awards.models import Award
 from ocds_contracts.models import Contract
+from ocds_planning.models import Planning
 
 from .constants import INITIATION_TYPE, PARTY_ROLE, RELEASE_TAG_CHOICES
 
 class Record(models.Model):
-    compiled_release = models.OneToOneField('Release', on_delete=models.DO_NOTHING)
+    compiled_release = models.OneToOneField('Release', on_delete=models.DO_NOTHING, null=True, blank=True)
 
 class Release(models.Model):
-    ref_record = models.ForeignKey(Record, on_delete=models.DO_NOTHING)
-    ocid = models.CharField(max_length=255)
-    date = models.DateTimeField()
+    ref_record = models.ForeignKey(Record, on_delete=models.DO_NOTHING, null=True, blank=True)
+    ocid = models.CharField(max_length=255, null=True, blank=True)
+    date = models.DateTimeField(null=True, blank=True)
     tag = models.CharField(max_length=255, choices=RELEASE_TAG_CHOICES)
     initiation_type = models.CharField(max_length=255, default='tender', choices=INITIATION_TYPE)
-    buyer = models.ForeignKey(Entity, on_delete=models.DO_NOTHING)
-    # planning = models.OneToOneField(Planning, on_delete=models.DO_NOTHING)
-    tender = models.OneToOneField(Tender, on_delete=models.DO_NOTHING)
+    buyer = models.ForeignKey(Entity, on_delete=models.DO_NOTHING, null=True, blank=True)
+    planning = models.OneToOneField(Planning, on_delete=models.DO_NOTHING, null=True, blank=True)
+    tender = models.OneToOneField(Tender, on_delete=models.DO_NOTHING, null=True, blank=True)
 
 class ReleaseAward(Award):
     ref_release = models.ForeignKey(Release, on_delete=models.DO_NOTHING)
