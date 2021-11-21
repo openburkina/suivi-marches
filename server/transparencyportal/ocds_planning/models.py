f2ecd75173b3a3a43518356d02222958e92cb406
Harry Michael <ando.lalaina1103@gmail.com>
2021-11-21 12:13:16 +0300

    Remove amount field on Planning

--- server/transparencyportal/ocds_planning/models.py
+++ server/transparencyportal/ocds_planning/models.py
@@ -6,9 +6,7 @@ from ocds_master_tables.models import Document, Budget, Value
 
 class Planning(models.Model):
     raison = models.TextField(null=True, blank=True)
-    budget = models.ForeignKey(Budget, on_delete=models.DO_NOTHING, null=True, blank=True)
-    amount = models.ForeignKey(Value, on_delete=models.DO_NOTHING, null=True, blank=True)
-
+    budget = models.OneToOneField(Budget, on_delete=models.DO_NOTHING, null=True, blank=True)
 
 class PlanningDocument(Document):
     planning = models.ForeignKey(Planning, on_delete=models.DO_NOTHING)
