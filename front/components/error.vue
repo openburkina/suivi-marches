49a668e967e6c47e5f19185aff2b99f2848b3614
AdamMusa <adammusa2222@gmail.com>
2021-11-22 17:18:52 +0100

    added icons in the menu option

--- front/components/error.vue
+++ front/components/error.vue
@@ -1,11 +1,20 @@
 <template>
-  <div>
-    <v-btn
-     color="primary"
-     dark
-     >
-       hello
-     </v-btn>
+  <div align="center" class="mb-5">
+      <v-img 
+        max-width="500"
+        :src="require('~/assets/img/adam.png')"
+      />
+      <v-btn
+        color="indigo"
+        dark
+        rounded
+        large
+        class="mt-3"
+        to="/"
+        >
+      <v-icon left large class="mx-5">mdi-arrow-left</v-icon>
+        <span class="font-weight-bold mx-5" id="spn">Go Home</span>   
+      </v-btn>  
   </div>
 </template>
 
@@ -14,3 +23,8 @@ export default {
 layout:'error'
 }
 </script>
+<style scoop>
+#spn{
+  font-size: 20px;
+}
+</style>
--- front/layouts/error.vue
+++ front/layouts/error.vue
@@ -1,23 +1,7 @@
 <template>
   <v-app dark>
     <v-container v-if="error.statusCode === 404">
-      <div align="center" class="mb-5">
-        <v-img 
-          max-width="500"
-          :src="require('~/assets/img/adam.png')"
-        />
-        <v-btn
-          color="indigo"
-          dark
-          rounded
-          large
-          class="mt-3"
-          to="/"
-          >
-        <v-icon left large class="mx-5">mdi-arrow-left</v-icon>
-        <span class="font-weight-bold mx-5" id="spn">Go Home</span>   
-        </v-btn>  
-      </div>
+      <Error />
     </v-container>
     <h1 v-else>
       {{ otherError }}
@@ -55,7 +39,5 @@ export default {
 h1 {
   font-size: 20px;
 }
-#spn{
-  font-size: 20px;
-}
+
 </style>
