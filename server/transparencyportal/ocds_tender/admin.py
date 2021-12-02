7242c606756e16e4cb198956e9d5d66d1c783764
Harry Michael <ando.lalaina1103@gmail.com>
2021-12-02 14:33:24 +0300

    Alter buyer field from Entity to Buyer

--- server/transparencyportal/ocds_tender/admin.py
+++ server/transparencyportal/ocds_tender/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Tender, TenderItem, TenderDocument, TenderMilestone, Tenderer
+from .models import Buyer, Tender, TenderItem, TenderDocument, TenderMilestone, Tenderer
+
+admin.site.register(Buyer)
 admin.site.register(Tender)
 admin.site.register(TenderItem)
 admin.site.register(TenderDocument)
