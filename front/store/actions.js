8a0203635d4b831213be0b3fec2539fee937d1f6
AdamMusa <adammusa2222@gmail.com>
2021-12-02 23:49:55 +0100

    d

--- front/store/actions.js
+++ front/store/actions.js
@@ -1,3 +1,8 @@
 // We put there all our actions, ie all our async functions(to request our api)
-
-export const myActions=({commit,id})=>{}
+import axios from 'axios'
+export const myActions=({commit})=>{
+    axios.get("http://0.0.0.0:8000/hello").then((res)=>{
+        console.log(res.data)
+        commit('getAllData',res.data)
+    })
+}
--- front/store/state.js
+++ front/store/state.js
@@ -1,5 +1,5 @@
 // We put there all our state, ie all our variables 
 
 export default{
-    msg:"Hello"
+    data:[]
 }
