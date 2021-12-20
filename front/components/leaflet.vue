833d02b82eabc287f3764ec65580b65d0ee72495
AdamMusa <adammusa2222@gmail.com>
2021-12-20 16:28:50 +0100

    fix bug from map

--- front/components/leaflet.vue
+++ front/components/leaflet.vue
@@ -2,43 +2,76 @@
     <div style="height: 80vh ;" class="mt-5">
         <client-only>
             <l-map :zoom=5 :center="[9.548624660911141, 7.804160193482599]" class="map">
-            <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></l-tile-layer>
-            <l-marker :lat-lng="[8.592724737747833, 16.074241050333463]" @mouseenter="hovering" @mouseleave="notHovering">
-               <l-popup >Hahha</l-popup>
-            </l-marker>
-    
-            <l-marker :lat-lng="[9.548624660911141, 7.804160193482599]" @mouseenter="hovering" @mouseleave="notHovering">
-                <l-popup>
-                    test 2
-                </l-popup>
-            </l-marker>
-            <l-marker :lat-lng="[12.125553758062672, 15.055388119977]" @mouseenter="hovering" @mouseleave="notHovering">
-                <l-popup>
-                    test 2
-                </l-popup>
-            </l-marker>
+                <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></l-tile-layer>
+                    <l-marker v-for="d in mock"  :lat-lng=d.lat @mouseenter="hovering(d.name)" @mouseleave="notHovering(d.name)" :ref="d.name" :key="d.id">
+                        <l-popup ref="popup">{{ d }}</l-popup>
+                    </l-marker>
+                 
             </l-map>
         </client-only>
-
     </div>
 </template>
-
 <script>
+
 export default {
     data(){
         return {
-           open:null
+           open:null,
+           mock:[
+               {
+                   id:0,
+                   msg:"hello from 0",
+                   name:'tchad',
+                   lat:[8.592724737747833, 16.074241050333463]
+
+               },
+                {
+                   id:1,
+                   msg:"hello from 1",
+                   name:'adams',
+                   lat:[9.548624660911141, 7.804160193482599]
+
+               },
+                 {
+                   id:2,
+                   msg:"hello from 2",
+                   name:'ndja',
+                   lat:[12.125553758062672, 15.055388119977]
+
+               },
+                {
+                   id:3,
+                   msg:"hello from 3",
+                   name:'adam',
+                   lat:[12.125553758062672, 15.055388119977]
+
+               },
+                {
+                   id:4,
+                   msg:"hello from 4",
+                   name:'nig',
+                   lat:[11.125553758062672, 12.055388119977]
+
+               }  
+           ]
         }
     },
-    mounted() {
-  },
   
   methods:{
-      hovering(){
-        
+  
+    hovering(name){
+        console.log(this.$refs.marker)
+        console.log(this.$refs[name][0])
+        this.$nextTick(() => {
+            this.$refs[name][0].mapObject.openPopup()
+
+        }) 
       },
-    notHovering(){
-         
+    notHovering(name){
+        console.log(name)
+        this.$nextTick(() => {
+            this.$refs[name][0].mapObject.closePopup()
+        }) 
       },
   }
 }
