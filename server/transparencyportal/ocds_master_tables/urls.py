f5e3060f0ca35fdd9d31bb7ec13e043bcbbe6933
Michael <ando.lalaina1103@gmail.com>
2022-01-30 00:36:40 +0300

    Add regions record-list endpoints

--- server/transparencyportal/ocds_master_tables/urls.py
+++ server/transparencyportal/ocds_master_tables/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import FacebookPublishView, RegionListView, RegionRecordListView
+from .views import FacebookPublishView, RegionListView, RegionRecordListView, RegionRecordNumberByStatusYearView, RegionRecordValueByGenericView, RegionRecordValueEvolutionBySectorView
 
 urlpatterns = [
         path(r'publish', FacebookPublishView.as_view()),
@@ -8,5 +8,8 @@ urlpatterns = [
 
 region_urlpatterns = [
         path(r'', RegionListView.as_view(), name='region-list'),
-        path(r'<int:region_id>/records', RegionRecordListView.as_view(), name='region-record-list')
+        path(r'<int:region_id>/records', RegionRecordListView.as_view(), name='region-record-list'),
+        path(r'<int:region_id>/records/sector_values', RegionRecordValueEvolutionBySectorView.as_view(), name='region-records-sector-values'),
+        path(r'<int:region_id>/records/values', RegionRecordValueByGenericView.as_view(), name='region-records-grouped-values'),
+        path(r'<int:region_id>/records/by_status', RegionRecordNumberByStatusYearView.as_view(), name='region-records-bystatus-list')
 ]
--- server/transparencyportal/ocds_master_tables/views.py
+++ server/transparencyportal/ocds_master_tables/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404, render
-from django.db.models import F, Count
+from django.db.models import F, Count, Sum, Value
+from django.db.models.functions import Concat
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -10,11 +11,14 @@ import requests
 from ocds_master_tables.serializers import RegionSerializer, RecordAggregateSerializer, RecordValueByGenericSerializer, RecordNumberByStatusYearSerializer, RecordValueBySectorYearSerializer, RecordValueEvolutionBySectorSerializer
 from ocds_release.models import Record, Release
 from ocds_master_tables.models import Address
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 from .constants import page_id_1, facebook_access_token_1
 # Create your views here.
 
 class RegionListView(APIView):
+    @swagger_auto_schema(responses={200:RegionSerializer(many=True)})
     def get(self, request):
         regions = Record.objects.values(
             'implementation_address__country_name',
@@ -27,6 +31,7 @@ class RegionListView(APIView):
         return Response(data)
 
 class RegionRecordListView(APIView):
+    @swagger_auto_schema(responses={200:RecordAggregateSerializer(many=True)})
     def get(self, request, region_id):
         region_instance = get_object_or_404(Address, pk=region_id)
         releases = Release.objects.filter(
@@ -42,7 +47,94 @@ class RegionRecordListView(APIView):
             )
         data = RecordAggregateSerializer(releases, many=True).data
         return Response(data)
+ 
+class RegionRecordNumberByStatusYearView(APIView):
+    @swagger_auto_schema(
+        responses={200:RecordNumberByStatusYearSerializer(many=True)},
+        manual_parameters=[
+            openapi.Parameter('year', openapi.IN_QUERY, type=openapi.TYPE_STRING)
+        ]
+    )
+    def get(self, request, region_id):
+        year = self.request.query_params.get('year')
+        if year is None:
+            return Response('Year not specified', status=500)
+        region_instance = get_object_or_404(Address, pk=region_id)
+        releases = Release.objects.filter(
+            ref_record__implementation_address = region_instance,
+            date__year=year
+        )
+        output = {
+            'planning': 0,
+            'tender': 0,
+            'award': 0,
+            'contract': 0,
+            'implementation': 0,
+            'done': 0,
+            'total': releases.count(),
+        }
+        for release in releases:
+            output[release.step] += 1
+        data = RecordNumberByStatusYearSerializer(output).data
+        return Response(data)
+ 
+class RegionRecordValueEvolutionBySectorView(APIView):
+    @swagger_auto_schema(
+        responses={200:RecordValueEvolutionBySectorSerializer(many=True)},
+        manual_parameters=[
+            openapi.Parameter('start_year', openapi.IN_QUERY, type=openapi.TYPE_STRING, required=True),
+            openapi.Parameter('end_year', openapi.IN_QUERY, type=openapi.TYPE_STRING, required=True)
+        ]
+    )
+    def get(self, request, region_id):
+        start_year = self.request.query_params.get('start_year')
+        end_year = self.request.query_params.get('end_year')
+        if start_year is None or end_year is None:
+            return Response('Year not specified', status=500)
+        if int(start_year) > int(end_year):
+            return Response('Start year is after end year', status=500)
 
+        region_instance = get_object_or_404(Address, pk=region_id)
+        records = Record.objects.filter(
+            implementation_address=region_instance,
+            compiled_release__date__year__gte=start_year,
+            compiled_release__date__year__lte=end_year,
+        ).values('compiled_release__date__year', sector=F('target__name'))\
+            .annotate(value=Sum('implementation_value__amount'), currency=F('implementation_value__currency'))
+        data = RecordValueEvolutionBySectorSerializer(records, many=True).data
+        return Response(data)
+ 
+class RegionRecordValueByGenericView(APIView):
+    @swagger_auto_schema(
+        responses={200:RecordValueByGenericSerializer(many=True)},
+        manual_parameters=[
+            openapi.Parameter('year', openapi.IN_QUERY, type=openapi.TYPE_STRING, required=True),
+            openapi.Parameter('group_by', openapi.IN_QUERY, type=openapi.TYPE_STRING, enum=['buyer', 'sector'], required=True)
+        ]
+    )
+    def get(self, request, region_id):
+        year = self.request.query_params.get('year')
+        group_by = self.request.query_params.get('group_by')
+        if year is None:
+            return Response('Year not specified', status=500)
+        if not group_by in ['buyer', 'sector']:
+            return Response('Group field not specified', status=500)
+
+        region_instance = get_object_or_404(Address, pk=region_id)
+        records = Record.objects.filter(
+                implementation_address=region_instance,
+                compiled_release__date__year=year
+            )
+        if group_by == 'buyer':
+            records = records.annotate(
+                name=F('compiled_release__buyer__name')
+            ).values('name')
+        if group_by == 'sector':
+            records = records.annotate(name=F('target__name')).values('name')
+        records = records.annotate(value=Sum('implementation_value__amount'), currency=F('implementation_value__currency'))
+        data = RecordValueByGenericSerializer(records, many=True).data
+        return Response(data)
+ 
 class FacebookPublishView(APIView):
     msg = "Ceci est un message test pour la publication des informations d'un marché publics"
     payload = {
